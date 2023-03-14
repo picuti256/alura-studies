5a679a517c6ccb9b1a538f67bc8ccc2079c8d78c
picuti256 <picuti256@gmail.com>
2023-03-14 08:51:50 -0300

    Criação do componente de cronometro + estilização

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,13 +1,15 @@
 import React from "react";
+import Cronometro from "../components/Cronometro";
 import Form from "../components/Form";
 import List from "../components/List";
-import style from './App.module.scss'
+import style from "./App.module.scss";
 
 function App() {
 	return (
 		<div className={style.AppStyle}>
 			<Form />
-      <List />
+			<List />
+			<Cronometro />
 		</div>
 	);
 }
