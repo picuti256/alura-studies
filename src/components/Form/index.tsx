1505909a1ae4430b2ab57c6f6f70917de62cb55c
picuti256 <picuti256@gmail.com>
2023-03-17 09:32:33 -0300

    Criação dos states do formulário

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,35 +1,33 @@
 import React from "react";
 import Button from "../Button";
-import style from './Form.module.scss'
+import style from "./Form.module.scss";
 
 class Form extends React.Component {
+	state = {
+		task: "",
+		time: "00:00",
+	};
+
+	addTask(e: React.FormEvent) {
+		e.preventDefault();
+		console.log("State: ", this.state);
+	}
 	render() {
 		return (
-			<form className={style.novaTarefa}>
+			<form className={style.novaTarefa} onSubmit={this.addTask.bind(this)}>
 				<div className={style.inputContainer}>
 					<label htmlFor="task">Adicione um novo estudo</label>
-					<input type="text" 
-                    name="task"
-                    id="task"
-                    placeholder="O que você quer estudar?"
-                    required/>
+					<input type="text" name="task" id="task" value={this.state.task} onChange={(e) => this.setState({ ...this.state, task: e.target.value })} placeholder="O que você quer estudar?" required />
 				</div>
 				<div className={style.inputContainer}>
 					<label htmlFor="time">Tempo</label>
-					<input type="time" step="1"
-                    id="time"
-                    name="time"
-                    min="00:00:00"
-                    max="01:30:00"
-                    required/>
+					<input type="time" step="1" id="time" name="time" value={this.state.time} onChange={(e) => this.setState({ ...this.state, time: e.target.value })} min="00:00:00" max="01:30:00" required />
 				</div>
 
-				<Button 
-					text="Adicionar"
-				/>
+				<Button text="Adicionar" />
 			</form>
 		);
 	}
 }
 
-export default Form
+export default Form;
